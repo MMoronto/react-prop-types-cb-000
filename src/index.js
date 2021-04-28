7a7c73301d7b22e8bbdb054124de5690a429ab2a
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-04-28 22:27:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -22,4 +22,12 @@ Order.defaultProps = {
   size: 'regular'
 };
  
-Order.propTypes = {};
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
